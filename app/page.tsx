9e8891dbee287f8ed7fd2e9b665347ceed7e8b55
Rasel Miah <rasel6041@gmail.com>
2025-01-22 14:00:20 +0100

    connection to database

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Button} from 'antd';
 import { UserButton } from "@clerk/nextjs";
 import {currentUser } from "@clerk/nextjs/server";
+import { connectMongoDB } from '@/config/db';
+connectMongoDB()
 export default async function Home() {
   let userId="";
   let name="";
   let email="";
   const currentUserData= await currentUser();
-  console.log(currentUserData)
+  /* console.log(currentUserData) */
   if(currentUserData){
     userId=currentUserData?.id;
     name=currentUserData?.firstName + " "+currentUserData?.lastName;
@@ -16,7 +18,7 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col items-center">
-      <h1>Welcome to Hotel Taha</h1>
+      <h1>Home Page</h1>
       <h1>Clerk:{userId}</h1>
       <h1>name: {name}</h1>
       <h1>email: {email}</h1>
