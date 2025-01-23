7c1fc726e188741a195c334773e52fa2f8c3cac4
Rasel Miah <rasel6041@gmail.com>
2025-01-23 08:38:01 +0100

    save current user into mongo db and fetch user

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,30 @@
 import { Button} from 'antd';
 import { UserButton } from "@clerk/nextjs";
-import {currentUser } from "@clerk/nextjs/server";
 import { connectMongoDB } from '@/config/db';
+import { GetCurrentUserFromMongoDB } from '@/server-actions/users';
 connectMongoDB()
 export default async function Home() {
-  let userId="";
+  const response:any=await GetCurrentUserFromMongoDB();
+  let mongouser=null;
+  if(response.success){
+    mongouser=response.data
+  }
+  let clerkUserId="";
   let name="";
   let email="";
-  const currentUserData= await currentUser();
+  
   /* console.log(currentUserData) */
-  if(currentUserData){
-    userId=currentUserData?.id;
-    name=currentUserData?.firstName + " "+currentUserData?.lastName;
-    email=currentUserData?.emailAddresses[0].emailAddress;
+  if(mongouser){
+    clerkUserId=mongouser.clerkUserId;
+    name=mongouser.name;
+    email=mongouser.email;
 
   }
 
   return (
     <div className="flex flex-col items-center">
       <h1>Home Page</h1>
-      <h1>Clerk:{userId}</h1>
+      <h1>Clerk:{clerkUserId}</h1>
       <h1>name: {name}</h1>
       <h1>email: {email}</h1>
       <Button type="default">Start</Button>
