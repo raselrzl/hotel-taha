34a523dd5b4c5eccb7395f34a4e1d7ee02a97c23
Rasel Miah <rasel6041@gmail.com>
2025-01-29 13:40:59 +0100

    admin module

---
+++ app/admin/hotels/add/page.tsx
@@ -0,0 +1,14 @@
+import PageTitle from "@/app/components/page-title";
+import React from "react";
+/* import HotelForm from "../_common/hotel-form"; */
+
+function AddHotelPage() {
+  return (
+    <div className="px-10">
+      <PageTitle title="Add Hotel" />
+     {/*  <HotelForm type="add" /> */}
+    </div>
+  );
+}
+
+export default AddHotelPage;
